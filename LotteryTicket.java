673a545a15a6acc33a7e0c61831b6215ede53cf1
Alexander Obregon <alexander.obregon47@gmail.com>
2023-02-11 14:57:06 -0800

    added another 2d array

--- LotteryTicket.java
+++ LotteryTicket.java
@@ -2,10 +2,13 @@ public class LotteryTicket {
 
 	public static void main(String[] args) {
         int [][] lotterycard = { {20, 15, 7},
-        {8, 7, 19},
-        {7, 13, 47}
+                                 {8, 7, 19},
+                                 {7, 13, 47}
 };
-		
+int [][] lotteryCard2 = new int[3][3];
+lotteryCard2[0][0] = 20;
+lotteryCard2[0][1] = 15;
+lotteryCard2[0][2] = 7;
 
 }
 }
